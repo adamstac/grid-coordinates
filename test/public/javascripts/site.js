8ec44a0ff348126ea28671ca0d57c42a929648c9
Adam Stacoviak <adam@stacoviak.com>
2011-10-25 02:30:20 -0500

    Serious refactoring. No breakage expected. Transitioned to SCSS, refactored repeating math logic to functions, refactored into beter code partials, added comments, added var to prefix the clearfix, updated the test project

--- test/public/javascripts/site.js
+++ test/public/javascripts/site.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
   // Create, show and hide the grid
   $('body').append('<div id="grid"></div>');
-  $('body').append('<a href="#" class="toggle-grid"><span class="hide">Hide Grid</span><span class="show">Show Grid</span></a>');
+  $('body').append('<a href="#" class="toggle-grid"><span class="show">Show Grid</span><span class="hide">Hide Grid</span></a>');
   $('a.toggle-grid').toggle(function() {
-    $('#grid').slideUp('slow');
-    $('.toggle-grid .hide').hide();
-    $('.toggle-grid .show').show();
-  }, function() {
     $('#grid').slideDown('fast');
     $('.toggle-grid .hide').show();
     $('.toggle-grid .show').hide();
+  }, function() {
+    $('#grid').slideUp('slow');
+    $('.toggle-grid .hide').hide();
+    $('.toggle-grid .show').show();
   });
 
 });
